078eea537279203331c04cef5ae0b59f8f5cf1dc
17937458081 <45580217+17937458081@users.noreply.github.com>
2019-01-28 18:57:11 +0800

    Create ApprovalConfiguration.cs

---
+++ Approval.Demo/Approval.Entity/ApprovalConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Approval.Entity
+{
+    /// <summary>
+    /// 审批流程配置表
+    /// </summary>
+  public  class ApprovalConfiguration
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int ConfigurationID { get; set; }
+        /// <summary>
+        /// 审批流程ID
+        /// </summary>
+        public int ProcessID { get; set; }
+        /// <summary>
+        /// 审批节点ID
+        /// </summary>
+        public int NodeID { get; set; }
+        /// <summary>
+        /// 审批人
+        /// </summary>
+
+        public int ApprovalUserID { get; set; }
+        /// <summary>
+        /// 下一步审批人
+        /// </summary>
+        public int NextApprovalUserID { get; set; }
+        /// <summary>
+        /// 审批状态
+        /// </summary>
+        public int StateID { get; set; }
+        /// <summary>
+        /// 审批条件
+        /// </summary>
+        public int ConditionID { get; set; }
+        /// <summary>
+        /// 是否是开头
+        /// </summary>
+        public int IsFirst { get; set; }
+    }
+}
