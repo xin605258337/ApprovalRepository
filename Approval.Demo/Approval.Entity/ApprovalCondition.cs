4d1abb55ae280fbe248b02349f8a19f2828bce9c
17937458081 <45580217+17937458081@users.noreply.github.com>
2019-01-28 19:10:09 +0800

    1.28

---
+++ Approval.Demo/Approval.Entity/ApprovalCondition.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Approval.Entity
+{
+    /// <summary>
+    /// --审批条件表
+    /// </summary>
+    public class ApprovalCondition
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int ConditionID { get; set; }
+        /// <summary>
+        /// 条件名称
+        /// </summary>
+        public string ConditionName { get; set; }
+    }
+}
---
+++ Approval.Demo/Approval.Entity/ApprovalState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Approval.Entity
+{
+    /// <summary>
+    /// 状态表
+    /// </summary>
+ public  class ApprovalState
+    {
+        /// <summary>
+        /// id
+        /// </summary>
+        public int StateID { get; set; }
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public string StateName { get; set; }
+    }
+}
