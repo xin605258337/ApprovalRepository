526bf8b7c0db88a50b81460cd72be4d3d1b7c979
YoonaZBJ <45481297+YoonaZBJ@users.noreply.github.com>
2019-01-28 20:43:36 +0800

    1.28-zbj01

---
+++ Approval.Demo/Approval.Api/Controllers/ApprovalConfigurationController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Approval.Api.Controllers
+{
+    using Approval.Entity;
+    using Approval.Services;
+    using Approval.IServices;
+    public class ApprovalConfigurationController : ApiController
+    {
+        public IApprovalServices ApprovalServices { get; set; }
+
+        /// <summary>
+        /// 添加审批配置
+        /// </summary>
+        /// <param name="approvalConfiguration"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Add")]
+        public int Add(ApprovalConfiguration approvalConfiguration)
+        {
+            return ApprovalServices.Add(approvalConfiguration);
+        }
+        /// <summary>
+        /// 获取审批配置信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetApprovalConfigurations")]
+        public List<ApprovalConfiguration> GetApprovalConfigurations()
+        {
+            return ApprovalServices.GetApprovalConfigurations();
+        }
+        /// <summary>
+        /// 删除审批配置信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Delete")]
+        public int Delete(int id)
+        {
+            return ApprovalServices.Delete(id);
+        }
+        /// <summary>
+        /// 获取审批条件信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetApprovalConditions")]
+        public List<ApprovalCondition> GetApprovalConditions()
+        {
+            return ApprovalServices.GetApprovalConditions();
+        }
+        /// <summary>
+        /// 获取审批节点信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetApprovalNodes")]
+        public List<ApprovalNode> GetApprovalNodes()
+        {
+            return ApprovalServices.GetApprovalNodes();
+        }
+        /// <summary>
+        /// 获取审批流程信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetApprovalProcesses")]
+        public List<ApprovalProcess> GetApprovalProcesses()
+        {
+            return ApprovalServices.GetApprovalProcesses();
+        }
+        /// <summary>
+        /// 获取审批状态信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetApprovalStates")]
+        public List<ApprovalState> GetApprovalStates()
+        {
+            return ApprovalServices.GetApprovalStates();
+        }
+        /// <summary>
+        /// 获取用户信息
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("GetUsers")]
+        public List<users> GetUsers()
+        {
+            return ApprovalServices.GetUsers();
+        }
+    }
+}
---
+++ Approval.Demo/Approval.Services/ApprovalServices.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Approval.Services
+{
+    using Approval.Entity;
+    using System.Data;
+    using System.Data.SqlClient;
+    using Dapper;
+    using Approval.IServices;
+    public class ApprovalServices:IApprovalServices
+    {
+        /// <summary>
+        /// 添加审批配置
+        /// </summary>
+        /// <param name="approvalConfiguration"></param>
+        /// <returns></returns>
+        public int Add(ApprovalConfiguration approvalConfiguration)
+        {
+            using (SqlConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = "insert into ApprovalConfiguration(ProcessID,NodeID,ApprovalUserID,NextApprovalUserID,StateID,ConditionID,IsFirst,UseUser) values(:ProcessID,:NodeID,:ApprovalUserID,:NextApprovalUserID,:StateID,:ConditionID,:IsFirst,:UseUser)";
+                return conn.Execute(sql, approvalConfiguration);
+            }
+        }
+        /// <summary>
+        /// 获取审批配置信息
+        /// </summary>
+        /// <returns></returns>
+        public List<ApprovalConfiguration> GetApprovalConfigurations()
+        {
+            using (SqlConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = "select c.*,a.ConditionName,n.NodeName,n.UserID,d.UserName,p.ProcessName,s.StateName,u.UserName,b.UserName from ApprovalConfiguration c inner join ApprovalCondition a on c.ConditionID = a.ConditionNameinner join ApprovalNode n on c.NodeID = n.NodeIDinner join users d on n.UserID = d.UserIDinner join ApprovalProcess p on c.ProcessID = p.ProcessIDinner join ApprovalState s on c.StateID = s.StateIDinner join users u on c.NextApprovalUserID = u.UserIDinner join users b on c.UseUser = b.UserID";
+                return conn.Query<ApprovalConfiguration>(sql, null).ToList();
+            }
+        }
+        /// <summary>
+        /// 删除审批配置信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public int Delete(int id)
+        {
+            using (SqlConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = "delete from ApprovalConfiguration where ConfigurationID=:ConfigurationID";
+                return conn.Execute(sql, new { ConfigurationID =id});
+            }
+        }
+        /// <summary>
+        /// 获取审批条件信息
+        /// </summary>
+        /// <returns></returns>
+        public List<ApprovalCondition> GetApprovalConditions()
+        {
+            using (SqlConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = "select * from ApprovalCondition";
+                return conn.Query<ApprovalCondition>(sql,null).ToList();
+            }
+        }
+        /// <summary>
+        /// 获取审批节点信息
+        /// </summary>
+        /// <returns></returns>
+        public List<ApprovalNode> GetApprovalNodes()
+        {
+            using (SqlConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = "select * from ApprovalNode";
+                return conn.Query<ApprovalNode>(sql, null).ToList();
+            }
+        }
+        /// <summary>
+        /// 获取审批流程信息
+        /// </summary>
+        /// <returns></returns>
+        public List<ApprovalProcess> GetApprovalProcesses()
+        {
+            using (SqlConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = "select * from ApprovalProcess";
+                return conn.Query<ApprovalProcess>(sql, null).ToList();
+            }
+        }
+        /// <summary>
+        /// 获取审批状态信息
+        /// </summary>
+        /// <returns></returns>
+        public List<ApprovalState> GetApprovalStates()
+        {
+            using (SqlConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = "select * from ApprovalState";
+                return conn.Query<ApprovalState>(sql, null).ToList();
+            }
+        }
+        /// <summary>
+        /// 获取用户信息
+        /// </summary>
+        /// <returns></returns>
+        public List<users> GetUsers()
+        {
+            using (SqlConnection conn = DapperHelper.GetConnString())
+            {
+                conn.Open();
+                string sql = "select * from users";
+                return conn.Query<users>(sql, null).ToList();
+            }
+        }
+    }
+}
